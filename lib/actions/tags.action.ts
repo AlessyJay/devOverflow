6e7dcda8d8abceb7c2e3289709ae099f8283a3b8
Tony Peeranat <TPeeranat@micamp.com>
2024-06-17 16:12:00 -0700

    Implement showing specific tag when clicked it will show the questions that has the tag

--- lib/actions/tags.action.ts
+++ lib/actions/tags.action.ts
@@ -1,9 +1,15 @@
 "use server";
 
-import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import {
+  GetAllTagsParams,
+  GetQuestionsByTagIdParams,
+  GetTopInteractedTagsParams,
+} from "./shared.types";
 import { connectToDB } from "../mongoose";
 import User from "@/database/user.model";
 import Tag from "@/database/tag.model";
+import { FilterQuery } from "mongoose";
+import Question from "@/database/question.model";
 
 export const getTopInteractedTags = async (
   params: GetTopInteractedTagsParams,
@@ -40,5 +46,41 @@ export const getAllTags = async (params: GetAllTagsParams) => {
   } catch (error) {
     console.log(error);
     return { tags: [] };
+  }
+};
+
+export const getSpecificTag = async (params: GetQuestionsByTagIdParams) => {
+  try {
+    connectToDB();
+
+    // eslint-disable-next-line no-unused-vars
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, "i") } }
+      : {};
+
+    const tag = await Tag.findOne({ tagId }).populate({
+      path: "question",
+      match: query,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!tagId) {
+      throw new Error("There is no such tag!");
+    }
+
+    const getTheTag = tag.saved;
+
+    return { tags: getTheTag };
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 };
