9d9db238a9f786110bd7e02f98acdd8409c83da1
BuildTools <unconfigured@null.spigotmc.org>
2024-06-25 22:09:20 -0700

    Starting to implement global search

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,8 +1,49 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { CircleX } from "lucide-react";
 
 const GlobalSearch = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const search = searchParams.get("global") || "";
+  // eslint-disable-next-line no-unused-vars
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>(search || "");
+
+  useEffect(() => {
+    const delayDebouncing = setTimeout(async () => {
+      if (inputValue) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: inputValue,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (search) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToremove: ["global", "type"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebouncing);
+  }, [search, router, pathname, searchParams, inputValue]);
+
+  const clearInput = () => {
+    setInputValue("");
+  };
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
@@ -16,9 +57,24 @@ const GlobalSearch = () => {
 
         <Input
           type="text"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            if (!isOpen) setIsOpen(true);
+            if (e.target.value === "" && isOpen) {
+              setIsOpen(false);
+            }
+          }}
           placeholder="Search Globally"
-          className="paragraph-regular no-focus placeholder background-light800_darkgradient text-dark200_light900 border-none shadow-none outline-none"
+          className="paragraph-regular no-focus placeholder background-light800_darkgradient text-dark400_light700 border-none shadow-none outline-none"
         />
+
+        {inputValue && (
+          <CircleX
+            className="text-dark300_light900 cursor-pointer"
+            onClick={clearInput}
+          />
+        )}
       </div>
     </div>
   );
